c667c41806daf7448e2061461421057f9fe996bf
Simba Mumanyi <simbamumanyi@gmail.com>
2020-07-14 16:13:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,21 @@ def batch_badge_creator(attendees)
     badge_maker(name)
   end
 end
-    
+  def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end  
 
   
