34ca21e7cfc83e0d0c806c3f9c76d0078e731c2d
Simba Mumanyi <simbamumanyi@gmail.com>
2020-07-14 16:02:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees)
   speakers.each do |name|
     badge_maker(name)
   end
